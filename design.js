9ef7eca5f59fa7ba7b46fe1b282020f374066ea8
Suriya-m-dev <124232046+Suriya-m-dev@users.noreply.github.com>
2023-07-14 19:46:13 +0530

    Add files via upload

---
+++ design.js
@@ -0,0 +1,20 @@
+var a=Math.floor(Math.random()*100)+1;
+var count=0;
+function guessNumber()
+{
+   var val=parseInt(document.getElementById("guessfield").value);
+   if(val==a)
+   {
+        document.getElementById("result").textContent = "Congratulations..! , You got the number in " +(count+1)+" tries.";
+   }
+   else if(val<a)
+   {
+        document.getElementById("result").textContent = "Try with a greater number";
+        count++;
+   }
+   else
+   {
+        document.getElementById("result").textContent = "Try with a smaller number";
+        count++;
+   }
+}
